34ea45b45a189463fa0d9eed549461a44bd7d182
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-22 17:28:09 -0600

    Add transform

--- datamodule.py
+++ datamodule.py
@@ -2,6 +2,7 @@ import torch
 import lightning as L
 from lightning.pytorch.utilities.types import TRAIN_DATALOADERS, EVAL_DATALOADERS
 from torch.utils import data
+from torchvision import transforms
 from torchvision import datasets
 from torch.utils.data import DataLoader
 class MNISTDataModule(L.LightningDataModule):
@@ -9,11 +10,11 @@ class MNISTDataModule(L.LightningDataModule):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
-        
+
     def setup(self, stage: str):
-        self.mnist_test = datasets.MNIST(self.data_dir, train=False)
-        self.mnist_predict = datasets.MNIST(self.data_dir, train=False)
-        mnist_full = datasets.MNIST(self.data_dir, train=True)
+        self.mnist_test = datasets.MNIST(self.data_dir, train=False, transform=transforms.ToTensor())
+        self.mnist_predict = datasets.MNIST(self.data_dir, train=False, transform=transforms.ToTensor())
+        mnist_full = datasets.MNIST(self.data_dir, train=True, transform=transforms.ToTensor())
         train_set_size = int(len(mnist_full) * 0.8)
         valid_set_size = len(mnist_full) - train_set_size
         self.mnist_train, self.mnist_val = data.random_split(
