33ed2b1e6779062782a605921dace7fd07347e40
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-21 19:03:49 -0600

    Separate train and test steps

--- test.py
+++ test.py
@@ -3,11 +3,8 @@ from model import Encoder, Decoder, LitAutoEncoder
 from dataloaders import train_loader, valid_loader, test_loader
 import lightning as L
 
-# Train model
-# model
 autoencoder = LitAutoEncoder(Encoder(), Decoder())
 
-# train model
-trainer = L.Trainer(devices=2, accelerator="gpu", max_epochs=1, default_root_dir="training")
-trainer.fit(autoencoder, train_loader, valid_loader)
+trainer = L.Trainer(devices=1, accelerator="gpu", num_nodes=1)
+trainer.test(model=autoencoder, dataloaders=test_loader)
 print("Done!")
