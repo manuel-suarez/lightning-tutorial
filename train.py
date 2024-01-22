030426ee3b620eb1b9fdc72f88b1dd431388c360
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-22 10:48:07 -0600

    Adding test step

--- train.py
+++ train.py
@@ -10,4 +10,8 @@ autoencoder = LitAutoEncoder(Encoder(), Decoder())
 # train model
 trainer = L.Trainer(devices=2, accelerator="gpu", max_epochs=5, default_root_dir="training")
 trainer.fit(autoencoder, train_loader, valid_loader)
-print("Done!")
+print("Training done!")
+
+trainer = L.Trainer(devices=1, accelerator="gpu", num_nodes=1)
+trainer.test(model=autoencoder, dataloaders=test_loader)
+print("Testing done!")
