49da5dfc45008b26401d0c3357bd03ea58dea749
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-22 18:06:18 -0600

    Use datamodule

--- train.py
+++ train.py
@@ -14,9 +14,9 @@ datamodule = MNISTDataModule(os.getcwd())
 
 # train model
 trainer = L.Trainer(max_epochs=5)
-trainer.fit(model, datamodule)
+trainer.fit(model, datamodule=datamodule)
 print("Training done!")
 
 # trainer = L.Trainer(devices=1, accelerator="gpu", num_nodes=1)
-trainer.test(model, datamodule)
+trainer.test(model, datamodule=datamodule)
 print("Testing done!")
